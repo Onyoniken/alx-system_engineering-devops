b5dac57560c6c5184fec08508472053d6960ee70
Onyoniken <onyonikenn@gmail.com>
2024-03-22 16:57:11 +0300

    0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # i awant to create a file at /tmp
 file { '/tmp/school':
-	ensure => 'file',
-	owner => 'www-data',
-	group => 'www-data',
-	mode => '0744',
-	content => 'I love Puppet',
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Executor of a ash command
 exec { 'kill':
-	command => 'pkill -f killmenow',
-	path => ['/usr/bin', '/usr/sbin']
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
